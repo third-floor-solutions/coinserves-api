4a7dd96c5db68608ffe421b8165a04ead0341af0
Nikku Ang <nikku.anthony.ang@gmail.com>
2019-10-01 15:58:04 +0800

    init

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Requests\Api\Auth\RegisterRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\Api\UserResource;
+use App\User;
+
+class RegisterController extends Controller
+{
+    //
+    public function __invoke(RegisterRequest $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save(); 
+
+        // Todo send mail
+        return new UserResource($user);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('api')->namespace('Api')->group(function() {
+    Route::namespace('Auth')->prefix('auth')->group(function(){
+        Route::post('login', 'AuthController@login');
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('register', 'RegisterController');
+
+        Route::get('me', 'AuthController@me');
+    });
+});
+ 
